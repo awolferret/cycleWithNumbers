ad81b7e5fdb3d83ebbdf34491fd203ec11778f31
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-06 21:51:21 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int amplifyNumber = 7;
+
+            for (number = 5; number < 100; number += amplifyNumber)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
